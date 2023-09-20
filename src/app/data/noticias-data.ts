825819b5b5b22c05d3fc1a0ae094578f728ff59d
ElkinFer <125397673+ElkinFer@users.noreply.github.com>
2023-09-20 00:25:06 -0500

    Add files via upload

---
+++ src/app/data/noticias-data.ts
@@ -0,0 +1,85 @@
+export const noticiasData = [
+  {
+    "noticias": {
+      "name": "All",
+      "date": "2023-07-30",
+      "show": true,
+      "categorys": [
+          {
+              "name": "Formación",
+              "value": "formacion"
+          },
+          {
+              "name": "Cordinación",
+              "value": "cordinacion"
+          },
+          {
+              "name": "Novedades",
+              "value": "novedades"
+          },
+          {
+              "name": "Otros",
+              "value": "otros"
+          }
+      ]
+
+  },
+  "detailNoticias": [
+      {
+          "title": "Gracias!!!",
+          "date": "2023-10-05",
+          "show": true,
+          "text_small": "¡¡¡Gracias!!! El SENA es la marca preferida de los colombianos",
+          "text_long": "¡Estamos orgullosos y emocionados! La formación de calidad y gratuita, la labor diaria de aprendices e instructores por el progreso de las regiones, el apoyo a quienes sueñan con tener empresa o a quienes buscan trabajo y la certificación de la experiencia son solo algunas de las razones por las que el Servicio Nacional de Aprendizaje (SENA) fue escogido como la marca nacional preferida por los colombianos.",
+          "url_imag": "./assets/icon/SENA-19092023.jpg",
+          "font_noticia":"https://pt.slideshare.net/DarioZea/escudo-y-logotipo-sena-42241637"
+      },
+      {
+          "title": "Cauca",
+          "date": "2023-10-05",
+          "show": true,
+          "text_small": "Desde el Cauca, el SENA le apuesta a la innovación y la competitividad a través de la ciencia",
+          "text_long":"Enfocarse en la ciencia a partir de la investigación, el desarrollo tecnológico e innovación, I+D+i, son retos diarios que asume el SENA Regional Cauca, a través de los semilleros de investigación, integrados por aprendices e instructores ávidos de conocimiento que van en búsqueda de soluciones a problemáticas de sus entornos. SENA Regional Cauca", 
+          "url_imag": "./assets/icon/Cauca1-18092023.png",
+          "font_noticia":"https://pt.slideshare.net/DarioZea/escudo-y-logotipo-sena-42241637"
+      },
+      {
+          "title": "Aprendices SENA",
+          "date": "2023-10-05",
+          "show": true,
+          "text_small": "Aprendices SENA producen hidrógeno verde y aportan a la justicia ambiental en Norte de Santander",
+          "text_long": "En un esfuerzo por aportar mano de obra calificada a la industria del calzado y proporcionar oportunidades laborales a jóvenes en Cali, el Centro de Diseño Tecnológico Industrial encabeza la formación a 21 aprendices en Guarnición de Calzado, para que posteriormente se vinculen a una reconocida empresa vallecaucana. Los participantes en el programa de formación de nivel operario están ampliando su conocimiento técnico, pero también mejorando sus habilidades blandas, herramientas esenciales para desempeñarse en esta área.",
+          "url_imag": "./assets/icon/Adobe_Express_20230916_1532410_1.png",
+          "font_noticia":"https://pt.slideshare.net/DarioZea/escudo-y-logotipo-sena-42241637"
+      },
+      {
+          "title": "HUERTA",
+          "date": "2023-10-05",
+          "show": true,
+          "text_small": "Impulso a la agricultura sostenible a través de huertas orgánicas en Huila",
+          "text_long": "En Pitalito (Huila), donde sobresale un majestuoso paisaje cafetero, el SENA Regional Huila está liderando un esfuerzo significativo para promover la agricultura sostenible en la región, mediante la implementación de huertas orgánicas. Este proyecto busca fortalecer la seguridad alimentaria, fomentar prácticas agrícolas respetuosas con el medio ambiente y brindar oportunidades de formación a los aprendices, proceso que busca ser replicado en toda la ruralidad del sur del Huila. Estas huertas no solo son ejemplo de prácticas agrícolas ecológicas, también sirven como el espacio didáctico en el que aprendices conocen sobre métodos de cultivo sostenible. Esto incluye el montaje y mantenimiento de cultivos sin el uso de pesticidas químicos ni fertilizantes sintéticos, lo que garantiza que se cosechen productos frescos y saludables.",
+          "url_imag": "./assets/icon/HUERTA.jpg",
+          "font_noticia":"https://pt.slideshare.net/DarioZea/escudo-y-logotipo-sena-42241637"
+      },
+      {
+          "title": "Moda SENA",
+          "date": "2023-10-05",
+          "show": true,
+          "text_small": "Puro talento antioqueño en el diseño de cuero, calzado y marroquinería",
+          "text_long": "Con diseños alegres y creativos que hicieron parte de la pasarela infantil y de adultos, se dio apertura al desfile de modas de aprendices SENA, que destacó 30 diseños de distintas formas y colores de finas costuras, algunos de ellos con técnicas de moda circular. Los integrantes del tecnólogo en Desarrollo de Colecciones para la Industria de la Moda en Sogamoso exhibieron ante un nutrido público sus creaciones, resultado de los aprendizajes adquiridos durante cuatro trimestres de formación que terminaron en una pasarela con dos colecciones: una de diseños infantiles y otra de adultos.",
+          "url_imag": "./assets/icon/Desfile1-12092023.jpg",
+          "font_noticia":"https://pt.slideshare.net/DarioZea/escudo-y-logotipo-sena-42241637"
+      },
+      {
+          "title": "SENA Virtual",
+          "date": "2023-10-05",
+          "show": true,
+          "text_small": "Cerca de 32.000 aprendices se conectan desde hoy a la familia SENA",
+          "text_long": "El Servicio Nacional de Aprendizaje (SENA) les da la bienvenida a 31.823 aprendices de todo el país que desde hoy comienzan su ciclo de formación virtual en 37 programas de los niveles técnico y tecnólogo. Dentro de los programas de nivel Tecnólogo que cuentan con mayor número de aprendices están Análisis y Desarrollo de Software, Gestión Agroempresarial, Gestión Integral del Transporte, Desarrollo Publicitario, Desarrollo de Medios Gráficos Visuales, Desarrollo Multimedia y Web y Animación 3D y Animación Digital. En cuanto a los programas de nivel Técnico figuran Atención Integral al Cliente, Programación de Software, Servicio de Recepción Hotelera, Asesoría Comercial, Proyectos Agropecuarios y Operaciones de Comercio Exterior, entre otros.",
+          "url_imag": "./assets/icon/Bienvenida-09112023.jpg",
+          "font_noticia":"https://pt.slideshare.net/DarioZea/escudo-y-logotipo-sena-42241637"
+      }
+  ]
+
+}
+]
